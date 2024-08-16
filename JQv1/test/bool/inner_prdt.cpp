2088fd24d7d43e57e8dd1f85ed82c8fe0a2fb7f1
studio <studio@studiodacstudio.mynet>
2024-08-16 06:26:13 -0700

    inner

--- JQv1/test/bool/inner_prdt.cpp
+++ JQv1/test/bool/inner_prdt.cpp
@@ -10,14 +10,18 @@ const int threads = 1;
 
 void test_inner_product(BoolIO<NetIO> *ios[threads], int party) {
   srand(time(NULL));
+  
   bool constant;
   bool *witness = new bool[2 * sz];
   memset(witness, 0, 2 * sz * sizeof(bool));
 
-  setup_zk_bool<BoolIO<NetIO>>(ios, threads, party);
-  sync_zk_bool<BoolIO<NetIO>>();
 
-  Bit *x = new Bit[2 * sz];
+  setup_zk_bool<BoolIO<NetIO>>(ios, threads, party);
+  
+  // sync_zk_bool<BoolIO<NetIO>>();
+
+  block *x = new block[2 * sz];
+  block *ab = new block[sz];
 
   if (party == ALICE) {
     bool sum = 0, tmp;
@@ -34,24 +38,41 @@ void test_inner_product(BoolIO<NetIO> *ios[threads], int party) {
   }
   ios[0]->flush();
 
-  for (int i = 0; i < 2 * sz; ++i)
-    x[i] = Bit(witness[i], ALICE);
+  random_bits_input<BoolIO<NetIO>>(party, x, 2 * sz);
 
-  sync_zk_bool<BoolIO<NetIO>>();
-  auto start = clock_start();
-  for (int j = 0; j < repeat; ++j) {
-    zkp_inner_prdt<BoolIO<NetIO>>(x, x + sz, constant, sz);
+  for (int i = 0; i < sz; ++i) {
+    ab[i] = auth_compute_and<BoolIO<NetIO>>(party, x[i], x[sz + i]);
   }
 
-  bool cheated = finalize_zk_bool<BoolIO<NetIO>>();
-  if (cheated)
-    error("cheated\n");
+  auto start = clock_start();
+  for (int i = 0; i < sz; ++i) {
+    if (party == ALICE) {
+      witness[i] =  getLSB(x[i]) ^ witness[i];
+      witness[sz + i] =  getLSB(x[sz + i]) ^ witness[sz + i];
+      ios[0]->send_bit(witness[i]);
+      ios[0]->send_bit(witness[sz + i]);
+    } else {
+      witness[i] = ios[0]->recv_bit();
+      witness[sz + i] = ios[0]->recv_bit();
+    }
+  }
+  
+  // sync_zk_bool<BoolIO<NetIO>>();
+  
+  for (int j = 0; j < repeat; ++j) {
+    zkp_inner_prdt<BoolIO<NetIO>>(x, x + sz, witness, witness + sz, ab, constant, sz);
+  }
+
 
   double tt = time_from(start);
   cout << "prove " << repeat << " degree-2 inner_product of length " << sz
        << endl;
   cout << "time use: " << tt / 1000 << " ms" << endl;
   cout << "average time use: " << tt / 1000 / repeat << " ms" << endl;
+
+  bool cheated = finalize_zk_bool<BoolIO<NetIO>>();
+  if (cheated)
+    error("cheated\n");
 
   delete[] witness;
   delete[] x;
@@ -75,7 +96,7 @@ int main(int argc, char **argv) {
               << std::endl;
     return -1;
   } else if (argc < 5) {
-    repeat = 100;
+    repeat = 30000000;
     sz = 10;
   } else {
     repeat = atoi(argv[3]);
