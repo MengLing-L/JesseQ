37632bc565d6bff4839e2f91f78813f364db0abb
MengLing-L <mengling@connect.hku.hk>
2024-08-29 04:08:17 -0700

    m

--- bw_run_party2.py
+++ bw_run_party2.py
@@ -2,7 +2,7 @@
 import subprocess
 
 ban = '''
-sudo tc qdisc add dev ens5 root tbf rate 200Mbit latency 15ms burst 375k
+sudo tc qdisc add dev ens5 root tbf rate 100Mbit latency 15ms burst 375k
 '''
 print(ban)
 subprocess.call(["bash", "-c", ban])
@@ -15,7 +15,7 @@ subprocess.call(["bash", "-c", JQv1_arith])
 
 ban = '''
 sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 400Mbit latency 15ms burst 750k
+sudo tc qdisc add dev ens5 root tbf rate 500Mbit latency 15ms burst 1125k
 '''
 print(ban)
 subprocess.call(["bash", "-c", ban])
@@ -26,48 +26,10 @@ JQv1_arith = '''
 print(JQv1_arith)
 subprocess.call(["bash", "-c", JQv1_arith])
 
-ban = '''
-sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 600Mbit latency 15ms burst 1125k
-'''
-print(ban)
-subprocess.call(["bash", "-c", ban])
-
-JQv1_arith = '''
-./JQv1/bin/test_arith_circuit_scalability 2 12345 172.31.28.201
-'''
-print(JQv1_arith)
-subprocess.call(["bash", "-c", JQv1_arith])
-
-ban = '''
-sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 800Mbit latency 15ms burst 1500k
-'''
-print(ban)
-subprocess.call(["bash", "-c", ban])
-
-JQv1_arith = '''
-./JQv1/bin/test_arith_circuit_scalability 2 12345 172.31.28.201
-'''
-print(JQv1_arith)
-subprocess.call(["bash", "-c", JQv1_arith])
 
 ban = '''
 sudo tc qdisc del dev ens5 root
 sudo tc qdisc add dev ens5 root tbf rate 1000Mbit latency 15ms burst 1875k
-'''
-print(ban)
-subprocess.call(["bash", "-c", ban])
-
-JQv1_arith = '''
-./JQv1/bin/test_arith_circuit_scalability 2 12345 172.31.28.201
-'''
-print(JQv1_arith)
-subprocess.call(["bash", "-c", JQv1_arith])
-
-ban = '''
-sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 1500Mbit latency 15ms burst 15000k
 '''
 print(ban)
 subprocess.call(["bash", "-c", ban])
--- bw_run_party2_jqv1_bool.py
+++ bw_run_party2_jqv1_bool.py
@@ -3,7 +3,19 @@ import subprocess
 
 ban = '''
 sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 40Mbit latency 15ms burst 75k
+sudo tc qdisc add dev ens5 root tbf rate 10Mbit latency 15ms burst 75k
+'''
+print(ban)
+subprocess.call(["bash", "-c", ban])
+JQv1_bool = '''
+./JQv1/bin/test_bool_circuit_scalability 2 12345 172.31.28.201
+'''
+print(JQv1_bool)
+subprocess.call(["bash", "-c", JQv1_bool])
+
+ban = '''
+sudo tc qdisc del dev ens5 root
+sudo tc qdisc add dev ens5 root tbf rate 20Mbit latency 15ms burst 75k
 '''
 print(ban)
 subprocess.call(["bash", "-c", ban])
@@ -16,7 +28,7 @@ subprocess.call(["bash", "-c", JQv1_bool])
 
 ban = '''
 sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 60Mbit latency 15ms burst 112.5k
+sudo tc qdisc add dev ens5 root tbf rate 30Mbit latency 15ms burst 75k
 '''
 print(ban)
 subprocess.call(["bash", "-c", ban])
@@ -29,59 +41,7 @@ subprocess.call(["bash", "-c", JQv1_bool])
 
 ban = '''
 sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 80Mbit latency 15ms burst 150k
-'''
-print(ban)
-subprocess.call(["bash", "-c", ban])
-
-JQv1_bool = '''
-./JQv1/bin/test_bool_circuit_scalability 2 12345 172.31.28.201
-'''
-print(JQv1_bool)
-subprocess.call(["bash", "-c", JQv1_bool])
-
-ban = '''
-sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 100Mbit latency 15ms burst 375k
-'''
-print(ban)
-subprocess.call(["bash", "-c", ban])
-
-JQv1_bool = '''
-./JQv1/bin/test_bool_circuit_scalability 2 12345 172.31.28.201
-'''
-print(JQv1_bool)
-subprocess.call(["bash", "-c", JQv1_bool])
-
-ban = '''
-sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 200Mbit latency 15ms burst 375k
-'''
-print(ban)
-subprocess.call(["bash", "-c", ban])
-
-JQv1_bool = '''
-./JQv1/bin/test_bool_circuit_scalability 2 12345 172.31.28.201
-'''
-print(JQv1_bool)
-subprocess.call(["bash", "-c", JQv1_bool])
-
-ban = '''
-sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 400Mbit latency 15ms burst 750k
-'''
-print(ban)
-subprocess.call(["bash", "-c", ban])
-
-JQv1_bool = '''
-./JQv1/bin/test_bool_circuit_scalability 2 12345 172.31.28.201
-'''
-print(JQv1_bool)
-subprocess.call(["bash", "-c", JQv1_bool])
-
-ban = '''
-sudo tc qdisc del dev ens5 root
-sudo tc qdisc add dev ens5 root tbf rate 600Mbit latency 15ms burst 1125k
+sudo tc qdisc add dev ens5 root tbf rate 50Mbit latency 15ms burst 112.5k
 '''
 print(ban)
 subprocess.call(["bash", "-c", ban])
@@ -98,5 +58,7 @@ sudo tc qdisc del dev ens5 root
 '''
 print(ban)
 subprocess.call(["bash", "-c", ban])
+
+
 
 
