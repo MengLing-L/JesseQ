d62c67420d1ef4e3a493b392dbec55c5633e4610
MengLing-L <mengling@connect.hku.hk>
2024-08-28 23:25:40 -0700

    m

--- bw_run_party2_jqv1_bool.py
+++ bw_run_party2_jqv1_bool.py
@@ -28,6 +28,32 @@ JQv1_bool = '''
 print(JQv1_bool)
 subprocess.call(["bash", "-c", JQv1_bool])
 
+ban = '''
+sudo tc qdisc del dev ens5 root
+sudo tc qdisc add dev ens5 root tbf rate 2000Mbit latency 15ms burst 3750k
+'''
+print(ban)
+subprocess.call(["bash", "-c", ban])
+
+JQv1_bool = '''
+./JQv1/bin/test_bool_circuit_scalability 2 12345 172.31.28.201
+'''
+print(JQv1_bool)
+subprocess.call(["bash", "-c", JQv1_bool])
+
+ban = '''
+sudo tc qdisc del dev ens5 root
+sudo tc qdisc add dev ens5 root tbf rate 5000Mbit latency 15ms burst 9375k
+'''
+print(ban)
+subprocess.call(["bash", "-c", ban])
+
+JQv1_bool = '''
+./JQv1/bin/test_bool_circuit_scalability 2 12345 172.31.28.201
+'''
+print(JQv1_bool)
+subprocess.call(["bash", "-c", JQv1_bool])
+
 
 ban = '''
 sudo tc qdisc del dev ens5 root
