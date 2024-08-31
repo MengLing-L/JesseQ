31a724d776f474a8bf043591d0d964c617ec8687
MengLing-L <mengling@connect.hku.hk>
2024-08-31 06:52:42 -0700

    m

--- batchman/bw_500M_party2.py
+++ batchman/bw_500M_party2.py
@@ -10,19 +10,19 @@ print(shell)
 subprocess.call(["bash", "-c", shell])
 
 shell = '''
-./stacking-vole-zk/build/bin/test_arith_stack_batched_matmul_v1 2 12345 172.31.19.139 16 512 1024
+./stacking-vole-zk/build/bin/test_arith_stack_batched_matmul_v1 2 12345 172.31.27.223 16 512 1024
 '''
 print(shell)
 subprocess.call(["bash", "-c", shell])
 
 shell = '''
-./JQv1/build/bin/test_arith_stack_batched_matmul_v1 2 12345 172.31.19.139 16 512 1024
+./JQv1/build/bin/test_arith_stack_batched_matmul_v1 2 12345 172.31.27.223 16 512 1024
 '''
 print(shell)
 subprocess.call(["bash", "-c", shell])
 
 shell = '''
-./JQv2/build/bin/test_arith_stack_batched_matmul_v1 2 12345 172.31.19.139 16 512 1024
+./JQv2/build/bin/test_arith_stack_batched_matmul_v1 2 12345 172.31.27.223 16 512 1024
 '''
 print(shell)
 subprocess.call(["bash", "-c", shell])
