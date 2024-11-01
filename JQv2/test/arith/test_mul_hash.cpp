ea5daff26f326d1c61b5911d904e61c1aa0ebce3
Ubuntu <ubuntu@ip-172-31-76-47.ec2.internal>
2024-11-01 08:12:53 +0000

    m

--- JQv2/test/arith/test_mul_hash.cpp
+++ JQv2/test/arith/test_mul_hash.cpp
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
   for (int j = 0; j < num_of_chunk; ++j) { 
     start = clock_start();
     for (int i = 0; i < (chunk); ++i) { 
-        pro = mult_mod(LOW64(a[i]), LOW64(pro));
+        pro = mult_mod(LOW64(a[i]), pro);
     }
     start1 += time_from(start);
   }
