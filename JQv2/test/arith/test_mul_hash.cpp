0fd231017e7e16eac8b2d508c4b53a0ae9368f5a
Yang Heng <yangheng@imac.mynet>
2024-11-06 18:37:13 +0800

    test mul

--- JQv2/test/arith/test_mul_hash.cpp
+++ JQv2/test/arith/test_mul_hash.cpp
@@ -31,6 +31,9 @@ int main(int argc, char **argv) {
   int len = 1000000;
   int chunk = 1000000;
   int num_of_chunk  = len / chunk;
+  blake3_hasher hasher;
+  blake3_hasher_init(&hasher);
+  uint8_t output[BLAKE3_OUT_LEN];
 
   __uint128_t* a = new __uint128_t[chunk];
   __uint128_t* b = new __uint128_t[chunk];
@@ -45,8 +48,8 @@ int main(int argc, char **argv) {
     //b[i] = rand() % PR;
   }
   if (party == ALICE){
+    start = clock_start();
     for (int j = 0; j < num_of_chunk; ++j) { 
-      start = clock_start();
       for (int i = 0; i < (chunk); ++i) { 
           pro = mult_mod(LOW64(a[i]), pro);
           // mult_mod(LOW64(a[i]), LOW64(b[i]));
@@ -54,8 +57,21 @@ int main(int argc, char **argv) {
       }
       start1 += time_from(start);
     }
+    cout << "\tMul Speed: \t" << start1<< "us \t" << endl;
+    start = clock_start();
+    for (int i = 0; i < num_of_chunk; ++i) { 
+      Hash::hash_for_block(a, 8 * chunk);
+    }
+    cout << "\tHash Speed: \t" << (time_from(start)) << "us \t" << endl;
+    
+    start = clock_start();
+    for (int i = 0; i < num_of_chunk; ++i) { 
+      blake3_hasher_update(&hasher, a, 8 * chunk);
+      blake3_hasher_finalize(&hasher, output, BLAKE3_OUT_LEN);
+    }
+    cout << "\tblake3 Hash Speed: \t" << (time_from(start)) << "us \t" << endl;
   }
-  cout << party << "\tMul Speed: \t" << start1<< "us \t" << endl;
+  
   // uint64_t* a = new uint64_t[chunk];
   // uint64_t* b = new uint64_t[chunk];
 
@@ -68,52 +84,41 @@ int main(int argc, char **argv) {
   //     mult_mod(b[i], a[i]);
   // }
   // cout << party << "\tMul Speed: \t" << (time_from(start)) << "us \t" << endl;
-  start = clock_start();
-  for (int i = 0; i < num_of_chunk; ++i) { 
-    Hash::hash_for_block(a, 8 * chunk);
-  }
-  cout << party << "\tHash Speed: \t" << (time_from(start)) << "us \t" << endl;
+  
 
-
-  blake3_hasher hasher;
-  blake3_hasher_init(&hasher);
-  uint8_t output[BLAKE3_OUT_LEN];
-  start = clock_start();
-  for (int i = 0; i < num_of_chunk; ++i) { 
-    blake3_hasher_update(&hasher, a, 8 * chunk);
-    blake3_hasher_finalize(&hasher, output, BLAKE3_OUT_LEN);
-  }
-  cout << party << "\tblake3 Hash Speed: \t" << (time_from(start)) << "us \t" << endl;
+  
  
   block *ab = new block[chunk];
   block *ab_ = new block[chunk];
   bos.random_bits_input(ab, chunk);
   
-  start = clock_start();
-  block tmp;
-  gfmul(ab[0], ab[1], &tmp);
-  for (int j = 0; j < num_of_chunk; ++j) { 
-    for(int i = 0; i < chunk; i++) {
-      gfmul(tmp, ab[i], &tmp);
-    }
-  }
-
-  cout << party << "\t Binary Mul Speed: \t" << (time_from(start)) << "us \t" << endl;
-
   
-  start = clock_start();
-  for (int i = 0; i < num_of_chunk; ++i) { 
-    Hash::hash_for_block(ab, sizeof(block) * chunk);
-  }
-  cout << party << "\tHash Speed: \t" << (time_from(start)) << "us \t" << endl;
+  if (party == ALICE){
+    start = clock_start();
+    block tmp;
+    gfmul(ab[0], ab[1], &tmp);
+    for (int j = 0; j < num_of_chunk; ++j) { 
+      for(int i = 0; i < chunk; i++) {
+        gfmul(tmp, ab[i], &tmp);
+      }
+    }
+
+    cout << "\t Binary Mul Speed: \t" << (time_from(start)) << "us \t" << endl;
+
+    start = clock_start();
+    for (int i = 0; i < num_of_chunk; ++i) { 
+      Hash::hash_for_block(ab, sizeof(block) * chunk);
+    }
+    cout << "\tHash Speed: \t" << (time_from(start)) << "us \t" << endl;
 
 
-  start = clock_start();
-  for (int i = 0; i < num_of_chunk; ++i) { 
-    blake3_hasher_update(&hasher, ab, sizeof(block) * chunk);
-    blake3_hasher_finalize(&hasher, output, BLAKE3_OUT_LEN);
+    start = clock_start();
+    for (int i = 0; i < num_of_chunk; ++i) { 
+      blake3_hasher_update(&hasher, ab, sizeof(block) * chunk);
+      blake3_hasher_finalize(&hasher, output, BLAKE3_OUT_LEN);
+    }
+    cout << "\tblake3 Hash Speed: \t" << (time_from(start)) << "us \t" << endl;
   }
-  cout << party << "\tblake3 Hash Speed: \t" << (time_from(start)) << "us \t" << endl;
   
 
   for (int i = 0; i < threads; ++i) {
