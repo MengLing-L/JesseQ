ac2ea7fa7a79aa0256205c2e4aa22e3ce6fcdc37
mengling <mengling@menglingdeiMac.local>
2024-11-11 14:50:41 +0800

    increase to 10^*

--- mul_hash_party1.py
+++ mul_hash_party1.py
@@ -19,6 +19,12 @@ JQv1_arith = '''
 print(JQv1_arith)
 subprocess.call(["bash", "-c", JQv1_arith])
 
+JQv1_arith = '''
+./JQv1/bin/test_arith_mul_hash_performance 1 12345 0 300000000 100000000
+'''
+print(JQv1_arith)
+subprocess.call(["bash", "-c", JQv1_arith])
+
 
 JQv1_arith = '''
 ./JQv1/bin/test_bool_mul_hash_performance 1 12345 0 300000 100000
@@ -34,6 +40,12 @@ subprocess.call(["bash", "-c", JQv1_arith])
 
 JQv1_arith = '''
 ./JQv1/bin/test_bool_mul_hash_performance 1 12345 0 30000000 10000000
+'''
+print(JQv1_arith)
+subprocess.call(["bash", "-c", JQv1_arith])
+
+JQv1_arith = '''
+./JQv1/bin/test_bool_mul_hash_performance 1 12345 0 300000000 100000000
 '''
 print(JQv1_arith)
 subprocess.call(["bash", "-c", JQv1_arith])
